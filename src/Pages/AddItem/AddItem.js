4e0a7b100cac30a34013a0d54900c6d46ecf929a
kamrul526628 <kamrulhasan526628@hmail.com>
2022-06-05 16:35:59 +0600

    6th commit

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -22,17 +22,24 @@ const AddItem = () => {
 
 
     return (
-        <div className='w-50 mx-auto'>
-            <h2>Please add a Item</h2>
-            <form className='form-control w-full max-w-xs' onSubmit={handleSubmit(onSubmit)}>
-                <input className='input input-bordered w-full max-w-xs' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
-                <textarea className='input input-bordered w-full max-w-xs' placeholder='Description' {...register("description")} />
-                <input className='input input-bordered w-full max-w-xs' placeholder='Price' type="number" {...register("price")} />
-                <input className='input input-bordered w-full max-w-xs' placeholder='Quantity' type="number" {...register("Quantity")} />
-                <input className='input input-bordered w-full max-w-xs' placeholder='Photo URL' type="text" {...register("img")} />
-                <input className='input input-bordered w-full max-w-xs' placeholder='Supplier Name' {...register("supplier_name", { required: true, maxLength: 20 })} />
-                <input type="submit" value="Add Item" />
-            </form>
+        <div className='flex h-screen justify-center items-center'>
+            
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                <h2 className='text-center text-2xl font-bold mb-3'>Please add a Item</h2>
+                <form className='form-control w-full max-w-xs' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='input input-bordered w-full max-w-xs mb-4' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                    <textarea className='input input-bordered w-full max-w-xs mb-4' placeholder='Description' {...register("description")} />
+                    <input className='input input-bordered w-full max-w-xs mb-4' placeholder='Price' type="number" {...register("price")} />
+                    <input className='input input-bordered w-full max-w-xs mb-4' placeholder='Quantity' type="number" {...register("Quantity")} />
+                    <input className='input input-bordered w-full max-w-xs mb-4' placeholder='Photo URL' type="text" {...register("img")} />
+                    <input className='input input-bordered w-full max-w-xs mb-4' placeholder='Supplier Name' {...register("supplier_name", { required: true, maxLength: 20 })} />
+                    <input className='btn w-full max-w-xs text-white' type="submit" value="Add Item" />
+                </form>
+                </div>
+                
+            </div>
+            
         </div>
     );
 };
