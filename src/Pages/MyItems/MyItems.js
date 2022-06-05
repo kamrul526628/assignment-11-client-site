250b3b0bdee21f2fb0937b21d16707f4f6b7016d
kamrul526628 <kamrulhasan526628@hmail.com>
2022-06-05 22:39:59 +0600

    6commit

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Items from '../Home/Items/Items';
 
 const MyItems = () => {
     return (
         <div>
-            
+            <Items></Items>
         </div>
     );
 };
